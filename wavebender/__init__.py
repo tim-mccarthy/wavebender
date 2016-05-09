1afb8e9351fc6f530d184a76971083fd405cbff0
Tim McCarthy <thoomfish@gmail.com>
2016-05-09 15:32:08 -0700

    Add waveform command line option

--- wavebender/__init__.py
+++ wavebender/__init__.py
@@ -128,11 +128,23 @@ def main():
     parser.add_argument('-t', '--time', help="Duration of the wave in seconds.", default=60, type=int)
     parser.add_argument('-a', '--amplitude', help="Amplitude of the wave on a scale of 0.0-1.0.", default=0.5, type=float)
     parser.add_argument('-f', '--frequency', help="Frequency of the wave in Hz", default=440.0, type=float)
+    parser.add_argument('-w', '--wave', help="The type of wave.", default='sine', type=str)
     parser.add_argument('filename', help="The file to generate.")
     args = parser.parse_args()
 
+    wave = None
+    if args.wave == 'sine':
+        wave = sine_wave
+    elif args.wave == 'square':
+        wave = square_wave
+    elif args.wave == 'triangle':
+        wave = triangle_wave
+    else:
+        print("Unknown wave type: {}".format(args.wave))
+        return
+
     # each channel is defined by infinite functions which are added to produce a sample.
-    channels = ((sine_wave(args.frequency, args.rate, args.amplitude),) for i in range(args.channels))
+    channels = ((wave(args.frequency, args.rate, args.amplitude),) for i in range(args.channels))
 
     # convert the channel functions into waveforms
     samples = compute_samples(channels, args.rate * args.time)
